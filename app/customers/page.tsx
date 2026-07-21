b5bb09b58435693b71f9e244611d10ee057eb5d6
eastonjeff <jweartmusic@gmail.com>
2026-07-20 22:38:21 -0500

    started implementing a data table

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,5 +1,41 @@
-export default function Customers() {
+import { DataTable } from "@/components/ui/data-table";
+import { Customer } from "@/lib/api/models/customer.dto";
+import { GetCustomersQuery } from "@/lib/api/models/customer.query";
+import { ColumnDef } from "@tanstack/react-table";
+
+async function GetCustomers(query:GetCustomersQuery): Promise<Customer[]> {
+  //TODO: replace with call to API service
+  return [
+    {
+      id: 1,
+      firstName: "Jeff",
+      lastName: "Easton"
+    }
+  ]
+}
+
+const columns:ColumnDef<Customer>[] = [
+  {
+    "accessorKey": "id",
+    "header": "Id"
+  },
+  {
+    "accessorKey": "firstName",
+    "header": "First Name"
+  },
+  {
+    "accessorKey": "lastName",
+    "header": "Last Name"
+  }
+]
+
+export default async function Customers() {
+  
+  const data = await GetCustomers({ pageNumber: 1, pageSize: 10 });
+
   return (
-    <div>Customers...</div>
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
   );
 }
---
+++ lib/api/models/customer.query.ts
@@ -0,0 +1,7 @@
+export type GetCustomersQuery = {
+    id?: number;
+    firstName?: string;
+    lastName?: string;
+    pageSize: number;
+    pageNumber: number;
+}
